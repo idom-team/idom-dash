dc7157c8021bfc326edd5be3367488ad18488496
rmorshea <ryan.morshead@gmail.com>
2022-01-19 21:18:45 -0800

    upgrade idom + fix usage example
    material ui seems to be broken right now on the CDN

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     include_package_data=True,
     license=package["license"],
     description=package.get("description", package_name),
-    install_requires=["idom[flask]>=0.32.0,<0.33.0"],
+    install_requires=["idom[flask]>=0.35,<0.36"],
     classifiers=[
         "Framework :: Dash",
     ],
